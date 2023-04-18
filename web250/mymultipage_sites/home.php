00882339a5bf8541675d46a21aa4ec6afcee94e4
gabi0521 <99023000+cgenife02@users.noreply.github.com>
2023-04-18 11:14:06 -0400

    add introForm

--- web250/mymultipage_sites/home.php
+++
@@ -1,7 +0,0 @@
-<div data-include="components/header.html"></div>
- <main>
-  <div class="fronth2">
-    <h2>Home Page</h2> 
-    <p class="pindex">Welcome to Sheeba Beauty</p>
-  </div>  
- </main> 
