970151328acc9ff32a06261d5206cf025e75de7a
gabi0521 <99023000+cgenife02@users.noreply.github.com>
2023-04-21 13:06:21 -0400

    introForm

--- web215/script/introductionForm.js
+++ web215/script/introductionForm.js
@@ -45,15 +45,25 @@ function displayEntry(){
     var Question5 = document.getElementById("qNum5").value;
     var Question6 = document.getElementById("qNum6").value;
     var Question7 = document.getElementById("qNum7").value;
+    var Question8 = document.getElementById("qNum8").value;
+    var Question9 = document.getElementById("qNum9").value;
+    var Question10 = document.getElementById("qNum10").value;
+   
     document.getElementById("display").innerHTML = fName+" "+mName+" "+lName;
-    document.getElementById("showAnswers1").innerHTML = "Personal Background:"+ Question1; 
-    document.getElementById("showAnswers2").innerHTML = "Professional Background:"+ Question2;
-    document.getElementById("showAnswers3").innerHTML = "Academic Background:"+ Question3;
-    document.getElementById("showAnswers4").innerHTML = "Information/Technology background:"+ Question4;
-    document.getElementById("showAnswers5").innerHTML = "Primary Computer Platform:"+ Question5;
-    document.getElementById("showAnswers6").innerHTML = "Courses I'm Taking & Why:"+ Question6;
-    document.getElementById("showAnswers7").innerHTML = "I'd also like to share:"+ Question7 ;
+    document.getElementById("showAnswers1").innerHTML = "Personal Background: "+ Question1; 
+    document.getElementById("showAnswers2").innerHTML = "Professional Background: "+ Question2;
+    document.getElementById("showAnswers3").innerHTML = "Academic Background: "+ Question3;
+    document.getElementById("showAnswers4").innerHTML = "Information/Technology background: "+ Question4;
+    document.getElementById("showAnswers5").innerHTML = "Primary Computer Platform: "+ Question5;
+    document.getElementById("showAnswers6").innerHTML = "Courses I'm Taking & Why:" + Question6;
+    document.getElementById("showAnswers7").innerHTML = " &nbsp;&nbsp;&nbsp;&nbsp; * " + Question7;
+    document.getElementById("showAnswers8").innerHTML = " &nbsp;&nbsp;&nbsp;&nbsp; * " + Question8;
+    document.getElementById("showAnswers9").innerHTML = " &nbsp;&nbsp;&nbsp;&nbsp; * " + Question9;
+    document.getElementById("showAnswers10").innerHTML = "I'd also like to share: "+ Question10 ;
+       
+}
+   
 
-}  
+
 
 
